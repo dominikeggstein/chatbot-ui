7c4d040c5c734106bc88ca0cd1e25414dab569f5
dominikeggstein <doeg-signup@outlook.com>
2023-04-04 23:36:42 +0200

    logo

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -154,6 +154,9 @@ export const Chat: FC<Props> = memo(
 
     return (
       <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
+        <div className="absolute top-0 left-0 opacity-90 z-10">
+        <img src="/Logo.png" alt="Logo" className="h-4 ml-12 mt-2.5" />
+        </div>
         {!(apiKey || serverSideApiKeyIsSet) ? (
           <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
             <div className="text-center text-4xl font-bold text-black dark:text-white">
